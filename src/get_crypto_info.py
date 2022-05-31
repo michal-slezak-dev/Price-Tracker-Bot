e4cea1cc1da7f33ec0511f1c741cf3e81d51db1c
Mike-920-dev <65869511+Mike920-dev@users.noreply.github.com>
2022-05-31 21:19:40 +0200

    refactor + added new functionalities (buy/sell -> you can see how much crypto u can buy and how much would you receive if you were to sell your crypto)

--- src/get_crypto_info.py
+++ src/get_crypto_info.py
@@ -15,9 +15,28 @@ def get_last_update(r: dict, crypto_name: str) -> str:  # get formatted date of 
     return convert_timestamp(r[crypto_name]["last_updated_at"])
 
 
-def convert_response_to_relevant_dict(mode: int, crypto_name=None, in_fiat=None) -> dict:
+def convert_if_zero(str_float: str) -> str:  # (convert) get rid of the part after '.' if this part consists of only '0'
+    return str_float.split(".")[0]
+
+
+def buy_sell_amount(price4one: float, buy_or_sell: bool = None, fiat_amount: float = None, amount: float = None) -> str:
+    result = ""
+    if buy_or_sell:  # True -> buy
+        result = "{:f}".format(fiat_amount / price4one).rstrip("0")
+
+    elif buy_or_sell == False:  # False -> sell
+        result = "{:f}".format(amount * price4one).rstrip("0")
+
+    if result != "" and result.split(".")[1] == '':
+        result = convert_if_zero(result)
+
+    return result
+
+
+def convert_response_to_relevant_dict(mode: int, crypto_name: str = None, in_fiat: str = None) -> dict:
     r = get_json_response(crypto_name, in_fiat)  # get API response
     crypto_dict = defaultdict(dict)  # get a new dict with shortened "crypto" name -> bitcoin => btc etc.
+
     if mode == 1:
         btc, eth, usd = "bitcoin", "ethereum", "usd"
         r[btc][f"{usd}_24h_change"], r[eth][f"{usd}_24h_change"] = get_24hr_change(r, btc, usd), get_24hr_change(r, eth, usd)
@@ -37,8 +56,7 @@ def convert_response_to_relevant_dict(mode: int, crypto_name=None, in_fiat=None)
         return dict(crypto_dict)
 
 
-def get_relevant_data(mode: int, crypto_name=None, in_fiat=None) -> str:
-    """TODO add a feature that will enable users to see how much crypto they can buy for a particular amount of money"""
+def get_relevant_data(mode: int, crypto_name: str = None, in_fiat: str = None, buy_or_sell: bool = None, fiat: float = None, crypto_amount: float = None) -> str:
     if mode == 1:  # daily tweet
         converted = convert_response_to_relevant_dict(1, "bitcoin,ethereum", "usd")
 
@@ -54,7 +72,7 @@ def get_relevant_data(mode: int, crypto_name=None, in_fiat=None) -> str:
         else:
             eth_change = f"🔺24hr ➡️ ️{eth_change}"
 
-        msg = f"BTC price at {converted[btc]['last_updated_at']}: {converted[btc][usd]} {btc_change}\n\n"
+        msg = f"BTC price at {converted[btc]['last_updated_at']}: {converted[btc][usd]} {btc_change}\n"
         msg += f"ETH price at {converted[eth]['last_updated_at']}: {converted[eth][usd]} {eth_change}"
 
         return msg
@@ -63,16 +81,29 @@ def get_relevant_data(mode: int, crypto_name=None, in_fiat=None) -> str:
         converted = convert_response_to_relevant_dict(2, crypto_name, in_fiat)
 
         crypto_change = converted[crypto_name][f'{in_fiat}_24h_change']
+        crypto_price = converted[crypto_name][in_fiat]
+
         if crypto_change < 0:
             change = f"🔻24hr ➡️ {crypto_change}"
         else:
             change = f"🔺24hr ➡️ {crypto_change}"
 
-        msg = f"{crypto_name} price at {converted[crypto_name]['last_updated_at']}: {converted[crypto_name][in_fiat]} {change}\n\n"
+        msg = f"{crypto_name} price at {converted[crypto_name]['last_updated_at']}: {crypto_price} {change}\n"
+        if buy_or_sell:  # True -> buy
+            buy = buy_sell_amount(crypto_price, True, fiat)
+            msg += f"For {fiat} {in_fiat.upper()}, you can buy: {buy} {crypto_name.upper()}"
+
+        elif buy_or_sell == False:  # False -> sell
+            sell = buy_sell_amount(crypto_price, False, amount=crypto_amount)
+            msg += f"After selling {crypto_amount} {crypto_name.upper()}, you'll receive {sell} {in_fiat.upper()}"
 
         return msg
 
-
+#  TODO add necessary comments so as to make the code more readable ;-)
 print(get_relevant_data(1))  # test mode 1
 print()
-print(get_relevant_data(2, "dogecoin", "usd"))  # test mode 2
+print(get_relevant_data(2, "dogecoin", "usd"))  # test mode 2 without "buy/sell"
+print()
+print(get_relevant_data(2, "dogecoin", "usd", True, 10))  # test mode 2 with "buy"
+print()
+print(get_relevant_data(2, "dogecoin", "usd", False, crypto_amount=116.061791))  # test mode 2 with "sell"
