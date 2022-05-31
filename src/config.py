790f15a2ec67505bf4dc86964baabe3b26ce49b1
Mike-920-dev <65869511+Mike920-dev@users.noreply.github.com>
2022-05-31 21:59:47 +0200

    set up condig file with .env values

--- src/config.py
+++ src/config.py
@@ -1,5 +1,11 @@
-from dotenv import dotenv_values
+from dotenv import load_dotenv
+import os
 
-config = {
-    **dotenv_values(".env")
-}
+load_dotenv()
+
+BEARER_TOKEN = os.getenv("BEARER_TOKEN")
+CONSUMER_KEY = os.getenv("CONSUMER_KEY")
+CONSUMER_SECRET = os.getenv("CONSUMER_SECRET")
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
+MY_ID = os.getenv("MY_ID")
