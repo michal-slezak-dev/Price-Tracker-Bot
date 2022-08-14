48eaf431866db6142b08708ec2d6f118b64ba9db
Mike-920-dev <65869511+Mike920-dev@users.noreply.github.com>
2022-08-14 21:02:53 +0200

    minor fixes

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Price-Tracker-Bot
  Bot that:
- 1. sends a message with information about the current price of BTC & ETH
+ 1. sends a message with information about the current price of BTC, ETH and other cryptocurrencies
  2. responds with the amount of a cryptocurrency that can be bought for a particular amount of money (FIAT currency)
  3. responds with the amount of money that could be received after selling a particular amount of crypto
--- src/config.py
+++ src/config.py
@@ -4,15 +4,3 @@ import os
 load_dotenv()
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-
-# BEARER_TOKEN = os.getenv("BEARER_TOKEN")
-# API_KEY = os.getenv("API_KEY")
-# API_SECRET = os.getenv("API_SECRET")
-# ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
-# ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
-# MY_ID = os.getenv("MY_ID")
-# CLIENT_ID = os.getenv("CLIENT_ID")
-# REDIRECT = os.getenv("REDIRECT")
-# SCOPES = "tweet.read,tweet.write,users.read,follows.read,follows.write,offline.access,like.read,like.write".split(",")
-# CLIENT_SECRET = os.getenv("CLIENT_SECRET")
-# RESPONSE_URL = os.getenv("RESPONSE_URL")
